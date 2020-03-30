cb982a23b4d766a162786f97ae2330763db56fcb
dengyu <deng.yu@arvato.com>
2020-03-30 15:23:03 +0800

    1

--- src/main/java/com/test/TestApplication.java
+++ src/main/java/com/test/TestApplication.java
@@ -3,9 +3,15 @@ package com.test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
 @MapperScan(value = {"com.test.vo"})
 @EnableScheduling
 public class TestApplication {
